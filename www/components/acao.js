0c8a313c19e9d653c9a8304f1cfe65524465b576
pedro1310 <phgarciarocha@gmail.com>
2020-05-28 04:34:03 +0900

    Project's Initial Commit

---
+++ www/components/acao.js
@@ -0,0 +1,33 @@
+// This is a JavaScript file
+
+$(document).on("click", "#btnSoma", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1) + parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+
+$(document).on("click", "#btnSub", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1) + parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+
+$(document).on("click", "#btnMult", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1) + parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
+
+$(document).on("click", "#btnDiv", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1) + parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
